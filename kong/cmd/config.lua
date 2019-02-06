a17a505fcac12d46e4a2270c084a545b663aad81
Enrique García Cota <kikito@gmail.com>
2019-02-06 19:32:59 +0100

    feat(cmd) add `kong config db_import` command
    This complements `kong config parse` with a command that imports
    a given file into the database.
    
    Note that this is a first version, and as such it does not support
    transactions yet (the command will stop and return if an error
    is detected when importing). Notice that since `:upsert` is used
    instead of `:insert`, some reentrancy is supported - however, in order
    for a config file to be reentrant, it needs to explicitly set the UUIDs
    for each entity it defines.

--- kong/cmd/config.lua
+++ kong/cmd/config.lua
@@ -1,5 +1,7 @@
+local DB = require "kong.db"
 local log = require "kong.cmd.utils.log"
 local pl_path = require "pl.path"
+local kong_global = require "kong.global"
 local declarative = require "kong.db.declarative"
 local conf_loader = require "kong.conf_loader"
 
@@ -27,6 +29,17 @@ local function execute(args)
   -- load <PREFIX>/kong.conf containing running node's config
   local conf = assert(conf_loader(default_conf.kong_env))
 
+  if args.command == "db-import" then
+    args.command = "db_import"
+  end
+
+  if args.command == "db_import" and conf.database == "off" then
+    log.error("'kong config db_import' only works with a database.")
+    log.error("When using database=off, reload your declarative configuration")
+    log.error("using the /config endpoint.")
+    os.exit(1)
+  end
+
   package.path = conf.lua_package_path .. ";" .. package.path
 
   local dc, err = declarative.init(conf)
@@ -35,7 +48,7 @@ local function execute(args)
     os.exit(1)
   end
 
-  if args.command == "parse" then
+  if args.command == "db_import" or args.command == "parse" then
     if not args.file then
       log("expected a declarative declarative configuration file; see `kong config --help`")
       os.exit(1)
@@ -48,8 +61,32 @@ local function execute(args)
       os.exit(1)
     end
 
-    log("parse successful:")
-    log(declarative.to_yaml_string(dc_table))
+    if args.command == "db_import" then
+      log("parse successful, beginning import")
+
+      _G.kong = kong_global.new()
+      kong_global.init_pdk(_G.kong, conf, nil) -- nil: latest PDK
+
+      local db = assert(DB.new(conf))
+      assert(db:init_connector())
+      assert(db:connect())
+      assert(db.plugins:load_plugin_schemas(conf.loaded_plugins))
+
+      _G.kong.db = db
+
+      local ok, err = declarative.load_into_db(dc_table)
+      if not ok then
+        log.error("Failed importing:")
+        log.error(err)
+        os.exit(1)
+      end
+
+      log("import successful")
+
+    else -- parse
+      log("parse successful:")
+      log(declarative.to_yaml_string(dc_table))
+    end
 
     os.exit(0)
   end
@@ -64,6 +101,9 @@ Usage: kong config COMMAND [OPTIONS]
 Use declarative configuration files with Kong.
 
 The available commands are:
+  db_import <file>              Import a declarative config file into
+                                the Kong database.
+
   parse <file>                  Parse a declarative config file (check
                                 its syntax) but do not load it into Kong.
 
